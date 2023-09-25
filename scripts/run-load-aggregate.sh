ba8cbea5767acffd8c603446fe8df470f9cf0690
Georgios Panayiotou <giorgospanay@gmail.com>
2023-09-25 11:39:44 +0200

    Initial

---
+++ scripts/run-load-aggregate.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# Experiment script for paper "A comparative study of libraries for quantitative 
+# multilayer network analysis". Providing a simple shellscript invoking the main 
+# functions from each library script, as they are sometimes coded on different 
+# languages. For more details, please refer to the documentation on each library's 
+# experiment script.
+# Author: Georgios Panayiotou
+
+
+# Change working directory
+cd ~/workspace/mln-lib-comp/scripts/
+
+# 
